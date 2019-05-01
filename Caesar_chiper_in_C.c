6206303bfa2f06332e92637ce282ea98874ed74b
Zahid-Hasan-Jony <zahidjony52@gmail.com>
2019-05-01 08:56:14 +0600

    Caesar cipher in c encryption and decryption

--- Caesar_chiper_in_C.c
+++ Caesar_chiper_in_C.c
@@ -11,43 +11,6 @@ int main()
    else printf("Enter a proper number");
    return 0;
 }
-char Decrypt(){
-  char meg[200], String;
-	int a, k;
-	
-        flush();
-	printf("Enter Cryptext: ");
-	fgets(meg,200,stdin);
-
-	printf("Enter key: ");
-	scanf("%d", &k);
-	
-	for(a = 0; meg[a] != '\0'; ++a){
-		String = meg[a];
-		
-		if( String>= 'a' && String <= 'z'){
-			String = String - k;
-			
-			if(String < 'a'){
-				String = String + 'z' - 'a' + 1;
-			}
-			
-			meg[a] = String;
-		}
-		else if(String >= 'A' && String <= 'Z'){
-			String = String - k;
-			
-			if(String < 'A'){
-				String = String + 'Z' - 'A' + 1;
-			}
-			
-			meg[a] = String;
-		}
-	}
-	
-	printf("Plaintext: %s", meg);
-
-  }
 
  int Encrypt(){
   char meg[200], String;
@@ -86,6 +49,45 @@ char Decrypt(){
 	printf("Cryptext: %s", meg);
 
   }
+
+ int Decrypt(){
+  char meg[200], String;
+	int a, k;
+	
+        flush();
+	printf("Enter Cryptext: ");
+	fgets(meg,200,stdin);
+
+	printf("Enter key: ");
+	scanf("%d", &k);
+	
+	for(a = 0; meg[a] != '\0'; ++a){
+		String = meg[a];
+		
+		if( String>= 'a' && String <= 'z'){
+			String = String - k;
+			
+			if(String < 'a'){
+				String = String + 'z' - 'a' + 1;
+			}
+			
+			meg[a] = String;
+		}
+		else if(String >= 'A' && String <= 'Z'){
+			String = String - k;
+			
+			if(String < 'A'){
+				String = String + 'Z' - 'A' + 1;
+			}
+			
+			meg[a] = String;
+		}
+	}
+	
+	printf("Plaintext: %s", meg);
+
+  }
+
 
  void flush(){
       int c;
